1028a582c24b23ca905890d56e0223679b15c565
TZP66 <TZP66@users.noreply.github.com>
2026-04-05 16:38:06 +0800

    Auto-publish web build 2026-04-05 16:38:05

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '20260405-desktop-fix-2';
+const CACHE_VERSION = '20260405-no-force-refresh-1';
 /** @type {string} */
 const CACHE_PREFIX = 'openclaw-web-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
