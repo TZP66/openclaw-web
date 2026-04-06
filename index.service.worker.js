bf7350c7df2d67d969944100df188f011170901f
TZP66 <TZP66@users.noreply.github.com>
2026-04-06 18:39:38 +0800

    Publish latest web build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775456195|5629036';
+const CACHE_VERSION = '1775469691|4876203';
 /** @type {string} */
 const CACHE_PREFIX = '钢翼秘术旅团-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
